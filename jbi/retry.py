be2e5d2b1882f6dd1b783aa09d8deff00cf6f154
Alex Cottner <acottner@mozilla.com>
2024-03-15 13:56:08 -0600

    working out unit tests

--- jbi/retry.py
+++ jbi/retry.py
@@ -2,47 +2,50 @@ import asyncio
 from datetime import datetime, timedelta
 from os import getenv
 from time import sleep
-
+import logging
+import jbi.runner as runner
 from jbi.configuration import ACTIONS
 from jbi.queue import QueueItem, get_dl_queue
-from jbi.runner import execute_action
 
 CONSTANT_RETRY = getenv("CONSTANT_RETRY", "false") == "true"
 RETRY_TIMEOUT_DAYS = getenv("RETRY_TIMEOUT_DAYS", 7)
-QUEUE = get_dl_queue()
+
+queue = get_dl_queue()
+logger = logging.getLogger(__name__)
 
 async def retry_failed():
   min_event_timestamp = datetime.now() - timedelta(days=int(RETRY_TIMEOUT_DAYS))
 
-  print('retry_failed 1')
-
   # load items from DLQ
-  items:list[QueueItem] = await QUEUE.retrieve()
-  
-  # track bugs that have failed
-  failed_bugs = {}
+  items:list[QueueItem] = await queue.retrieve()
 
-  for item in items: 
+  # track bugs that have failed
+  failed_bugs = dict()
+
+  for item in items:
     bug = item.payload.bug
 
     # skip and delete if we have exceeded max_timeout
     if item.timestamp < min_event_timestamp:
-      print("removing expired event for bug " + bug.id)
-      QUEUE.done(item)
-      continue
-    
-    # skip if any previous retries for this bug have already failed
-    if failed_bugs[bug.id]:
-      print("skipping bug " + bug.id)
+      logger.warn("removing expired event %d", item.identifier)
+      await queue.done(item)
       continue
 
-    try:
-      execute_action(item.payload, ACTIONS)
-      QUEUE.done(item)
-    except ex:
-      # write well formed log that could be alerted on
-      failed_bugs[bug.id] = True
-      print('bar')
+    # skip if any previous retries for this bug have already failed
+    if bug.id in failed_bugs:
+      logger.info("skipping event %d - previous items have failed", item.identifier)
+      continue
+
+    # try:
+    print(runner.execute_action)
+    runner.execute_action(item.payload, ACTIONS)
+    await queue.done(item)
+    # except Exception as ex:
+    #   print(ex)
+    #   # write well formed log that could be alerted on
+    #   failed_bugs[bug.id] = True
+    #   logger.error(ex)
+
 
 async def main():
   while True:
@@ -50,7 +53,8 @@ async def main():
 
     if not CONSTANT_RETRY:
       return
-    
+
     sleep(5)
 
-asyncio.run(main())
+if __name__ == '__main__':
+  asyncio.run(main())
--- tests/unit/test_retry.py
+++ tests/unit/test_retry.py
@@ -1,19 +1,80 @@
-
 from jbi.retry import retry_failed
+from unittest.mock import AsyncMock, MagicMock
+import asyncio
+from jbi.queue import QueueItem, get_dl_queue
+import pytest
+import logging
+import jbi.runner
+from datetime import datetime
+from jbi.bugzilla.models import Bug, WebhookRequest, WebhookEvent
 
-# def test_setup_retry_schedule():
-#   schedule.every = MagicMock()
-  
-#   # Verifying that if FAST_RETRY is set to "true", it's scheduled every 5 seconds
-#   os.environ['FAST_RETRY'] = 'true'
-#   setup_retry_schedule()
-#   schedule.every().seconds.assert_called_once_with(5)
-  
-#   # Verifying if FAST_RETRY is not set to "true", it runs at 2:00 and 14:00
-#   os.environ['FAST_RETRY'] = 'false'
-#   setup_retry_schedule()
-#   schedule.every().day.at.assert_has_calls([call("02:00"), call("14:00")], True)
+@pytest.fixture()
+def logger():
+    log_info = MagicMock()
+    log_warn = MagicMock()
+    log_error = MagicMock()
+    logger = logging.getLogger(__name__)
+    logger.info = log_info
+    logger.warn = log_warn
+    logger.error = log_error
+    return logger
 
-def test_retry_failed():
-  retry_failed()
+
+@pytest.fixture()
+def execute_action():
+    _execute_action = MagicMock()
+    jbi.runner.execute_action = _execute_action
+    return _execute_action
+
+
+@pytest.mark.asyncio
+async def test_retry_empty_list(
+    logger
+):
+    retrieve = AsyncMock(return_value=[])
+    get_dl_queue().retrieve = retrieve
+
+    await retry_failed()
+    retrieve.assert_called_once()
+    logger.info.assert_not_called()
+    logger.warn.assert_not_called()
+    logger.error.assert_not_called()
+
+
+@pytest.mark.asyncio
+async def test_retry_success(
+    logger,
+    execute_action
+):
+    queue = get_dl_queue()
+    queue.retrieve = AsyncMock(
+        return_value=[
+            QueueItem(
+                timestamp=datetime.now(),
+                payload=WebhookRequest(
+                    webhook_id = 1,
+                    webhook_name = "test",
+                    bug=Bug(id=1), 
+                    event=WebhookEvent(action="test")
+                ),
+            )
+        ]
+    )
+    queue.done = AsyncMock()
+    
+    await retry_failed()
+    queue.retrieve.assert_called_once()
+    queue.done.assert_called_once()
+    logger.info.assert_not_called()
+    logger.warn.assert_not_called()
+    logger.error.assert_not_called()
+    execute_action.assert_called_once()
+
+
+# @pytest.mark.asyncio
+# async def test_retry_fail_and_skip():
+
+
+# @pytest.mark.asyncio
+# async def test_retry_remove_expired():
 
